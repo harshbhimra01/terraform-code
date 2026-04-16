3b7ddbd2ab13cb2c4fd6f620997441ed30e6733b
harshbhimra01 <harsh.bhimra@gmail.com>
2026-04-16 13:34:21 +0530

    added main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "ball" {
+  name     = "harsh-resources"
+  location = "south india"
+}
+
+resource "azurerm_storage_account" "implicit" {
+  name                     = "harshchetansujeet"
+  resource_group_name      = azurerm_resource_group.ball.name
+  location                 = azurerm_resource_group.ball.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+resource "azurerm_storage_account" "explicit" {
+  depends_on               = [azurerm_resource_group.ball]
+  name                     = "harshchetansujeet1"
+  resource_group_name      = "harsh-resources"
+  location                 = "south india"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
